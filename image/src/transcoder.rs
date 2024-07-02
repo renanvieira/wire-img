4838643d2c7e3435cdffe8b3db0d57aee00a0cf6
Renan Chagas <renanvieira@users.noreply.github.com>
2024-07-02 13:45:54 +0200

    Basic features for image processing (#2)
    * adds the on-the-fly basic image processing with resize op
    
    * remove exlucde from image crate cargo.toml
    
    * make anyhow workspace dependency
    
    * implements crop operation
    
    ---------
    
    Co-authored-by: Renan Vieira <2496686+renanvieira@users.noreply.github.com>

---
+++ image/src/transcoder.rs
@@ -0,0 +1,227 @@
+use std::io::Cursor;
+
+use anyhow::anyhow;
+use image::{guess_format, ImageFormat};
+
+#[derive(Debug)]
+pub enum ImageEncoding {
+    AVIF,
+    JPEG,
+    PNG,
+}
+
+#[derive(Debug)]
+pub struct Position(u32, u32);
+
+impl Position {
+    pub fn x(&self) -> &u32 {
+        &self.0
+    }
+
+    pub fn y(&self) -> &u32 {
+        &self.1
+    }
+}
+
+#[derive(Debug)]
+pub struct PixelSize(u32, u32);
+
+impl PixelSize {
+    pub fn width(&self) -> &u32 {
+        &self.0
+    }
+
+    pub fn height(&self) -> &u32 {
+        &self.1
+    }
+}
+
+#[derive(Debug)]
+pub enum Operations {
+    Resize(PixelSize),
+    Crop(Position, PixelSize),
+}
+
+impl ImageEncoding {
+    pub fn content_type(&self) -> &str {
+        match self {
+            ImageEncoding::AVIF => "image/avif",
+            ImageEncoding::JPEG => "image/jpeg",
+            ImageEncoding::PNG => "image/png",
+        }
+    }
+}
+pub trait Encoder {
+    fn transcode(
+        &self,
+        image: &[u8],
+        target: ImageFormat,
+        ops: Option<Vec<Operations>>,
+    ) -> anyhow::Result<Vec<u8>>;
+}
+
+#[derive(Default)]
+pub struct Transcoder;
+
+impl Encoder for Transcoder {
+    fn transcode(
+        &self,
+        image: &[u8],
+        target: ImageFormat,
+        ops: Option<Vec<Operations>>,
+    ) -> anyhow::Result<Vec<u8>> {
+        let format_result = guess_format(image);
+
+        match format_result {
+            Ok(format) => format,
+            Err(err) => {
+                return Err(anyhow!(
+                    "error while trying to validate image format: {:?}",
+                    err
+                ))
+            }
+        };
+
+        let bytes: Vec<u8> = Vec::new();
+        let mut cursor = Cursor::new(bytes);
+        let mut image = image::load_from_memory(image)?;
+
+        if let Some(operations) = ops {
+            for op in operations {
+                match op {
+                    Operations::Resize(s) => {
+                        image = image.resize(
+                            *s.width(),
+                            *s.height(),
+                            image::imageops::FilterType::CatmullRom,
+                        );
+                    }
+                    Operations::Crop(p, s) => {
+                        image = image.crop_imm(*p.x(), *p.y(), *s.width(), *s.height());
+                    }
+                }
+            }
+        }
+
+        image.write_to(&mut cursor, target)?;
+        Ok(cursor.get_ref().to_vec())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{env, fs, path::Path};
+
+    use image::DynamicImage;
+
+    use super::*;
+
+    fn get_png_image() -> Vec<u8> {
+        let s = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let p = Path::new(&s).join("../resources/10x10.png");
+
+        fs::read(p).unwrap()
+    }
+
+    fn get_jpeg_image() -> Vec<u8> {
+        let s = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let p = Path::new(&s).join("../resources/100x100.jpg");
+
+        fs::read(p).unwrap()
+    }
+
+    fn get_avif_image() -> Vec<u8> {
+        let s = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let p = Path::new(&s).join("../resources/fox.avif");
+
+        fs::read(p).unwrap()
+    }
+
+    fn transcode(
+        img: Vec<u8>,
+        output_format: ImageFormat,
+        ops: Option<Vec<Operations>>,
+    ) -> anyhow::Result<DynamicImage> {
+        let t = Transcoder::default();
+        let output_img = t.transcode(&img, output_format, ops)?;
+
+        assert!(output_img.len() > 0);
+
+        Ok(image::load_from_memory_with_format(
+            &output_img,
+            output_format,
+        )?)
+    }
+
+    #[test]
+    fn test_transcoder_avif_to_png() -> anyhow::Result<()> {
+        let avif_img = get_avif_image();
+
+        assert!(guess_format(&avif_img)? == ImageFormat::Avif);
+
+        let output_img = transcode(avif_img, ImageFormat::Png, None)?;
+
+        assert_eq!(output_img.width(), 1204);
+        assert_eq!(output_img.height(), 800);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_transcoder_avif_to_png_with_resize() -> anyhow::Result<()> {
+        let avif_img = get_avif_image();
+
+        assert!(guess_format(&avif_img)? == ImageFormat::Avif);
+
+        let ops = vec![Operations::Resize(PixelSize(602, 400))];
+        let output_img = transcode(avif_img, ImageFormat::Png, Some(ops))?;
+
+        assert_eq!(output_img.width(), 602);
+        assert_eq!(output_img.height(), 400);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_transcoder_png_to_avif() -> anyhow::Result<()> {
+        let png_img = get_png_image();
+
+        assert!(guess_format(&png_img)? == ImageFormat::Png);
+
+        let img = transcode(png_img, ImageFormat::Avif, None)?;
+
+        assert_eq!(img.width(), 20);
+        assert_eq!(img.height(), 20);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_transcoder_jpeg_to_avif_with_resize() -> anyhow::Result<()> {
+        let jpg_img = get_jpeg_image();
+
+        assert!(guess_format(&jpg_img)? == ImageFormat::Jpeg);
+
+        let ops = vec![Operations::Resize(PixelSize(50, 50))];
+
+        let img = transcode(jpg_img, ImageFormat::Avif, Some(ops))?;
+
+        assert_eq!(img.width(), 50);
+        assert_eq!(img.height(), 50);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_transcoder_jpeg_to_avif() -> anyhow::Result<()> {
+        let jpg_img = get_jpeg_image();
+
+        assert!(guess_format(&jpg_img)? == ImageFormat::Jpeg);
+        let img = transcode(jpg_img, ImageFormat::Avif, None)?;
+
+        assert_eq!(img.width(), 100);
+        assert_eq!(img.height(), 100);
+
+        Ok(())
+    }
+}
